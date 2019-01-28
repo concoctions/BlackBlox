bf550105e00fedac78c987c07d40174358b86d86
Wajju <47030036+concoctions@users.noreply.github.com>
2019-01-28 18:11:14 +0100

    Added column header variables to config file

---
+++ Refactor/BBconfig.py
@@ -0,0 +1,39 @@
+from IOfunctions import makeDF
+
+# see README.txt for specified file foramts
+
+# File Settings
+
+ 
+# get list of unit processes
+unitListTSV = 'unitList.tsv'        # list of unit processes
+df_unitList = makeDF(unitListTSV)
+
+# specify unitListTSV column headers:
+ul_name = 'name'
+ul_product = 'product'
+ul_productIO = 'productType'
+ul_varFileLoc = 'varFile'
+ul_calcFileLoc = 'calcFile'
+
+# specify calculation file column headers:
+
+c_known = 'knownQty'
+c_kFrom = 'k_QtyFrom'
+c_unknown = 'UnknownQty'
+c_uTo = 'u_QtyTo'
+c_calcType = 'Calculation'
+c_var = 'Variable'
+
+
+# Specify lookup variables (if seen in calc file, replaces with specific value from variable file)
+lookupVar = {
+    'fuel': (makeDF('fuels.tsv'), 'fuelType')  #keyword string in calc file to trigger lookup: (dataframe of lookup data, column name in variable table to replace with) 
+    }
+
+# Specify shortcut names for lookup dataFrames:
+df_fuels = lookupVar['fuel'][0]
+
+
+
+
