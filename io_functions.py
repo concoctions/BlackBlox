9739c4447ad0d730eb2f70bb0e28a7b932fa1419
Wajju <setanzer@gmail.com>
2019-03-11 14:43:42 +0100

    reordered file structure

--- io_functions.py
+++ io_functions.py
@@ -6,14 +6,23 @@ retrieve user input and manipulate it into useable formats, as well as
 functions used in output file generation.
 
 Module Outline:
-- imports
-- function: make_df
+
+Input Data Validators & Cleaners
+- function: clean_str
+- function: if_str
 - function: check_for_col
+- function: is_energy
+Data Frame Constructors
+- function: make_df
+- function: mass_energy_df
+- function: metadata_df
+Writers to Files
 - function: build_filedir
 - function: write_to_excel
-- fucntion: sl
-- function: if_str
-- function: nested_dicts
+- function: format_and_save_plot
+- function: plot_annual_flows
+Miscellaneous Functions
+- nested_dicts
 
 """
 
@@ -25,9 +34,120 @@ from collections import defaultdict, OrderedDict
 from datetime import datetime
 import dataconfig as dat
 from bb_log import get_logger
+import about
 
 logger = get_logger("IO")
 
+
+# INPUT DATA VALIDATORS AND CLENAERS
+
+def clean_str(string_to_check, str_to_cut=False, lower=True, remove_dblnewline=True):
+    """Multipurpose function to clean user input strings
+    
+    Used to clean user input. First creates a copy of the string to prevent 
+    the original string from being modified unintentionally.
+
+    Args:
+        string_to_check (str): The string to strip and lower
+        str_to_cut (str/list/bool): If passed a string, will check for and 
+            remove the cut_str string from the string_to_check. If passed a list 
+            of strings, will check for and remove each from the string_to_check.
+            (Defaults to False.)
+        remove_dblnewlines (bool): If True, looks for and removes instances
+            of double new lines.
+            (Defaults to True.)
+
+    Returns
+        The input string, stripped of leading and trailing white space 
+        and in all lowercase.
+
+    """
+
+    string = ''.join(string_to_check)
+
+    string = string.strip()
+    
+    if lower is True:
+        string = str.lower(string)
+
+    if type(str_to_cut) is str:
+        string = string.replace(str_to_cut, '')
+    if type(str_to_cut) is list:
+        for snip in str_to_cut:
+            string = string.replace(snip, '')
+
+    if remove_dblnewline is True:
+        if '\n\n' in string:
+            string = string.replace('\n\n', '\n')
+
+    return string
+
+
+def if_str(maybe_a_string):
+    """checks if an input is a string, returns string if so, None if not.
+
+    Args:
+        string: a thing to check if it's a string.
+    
+    Returns:
+        The string, if it was a string, otherwise returns None.
+
+    """
+
+    if type(maybe_a_string) is str:
+        return maybe_a_string
+    else:
+        return None
+
+
+def check_for_col(df, col, index):
+    """ Checks if a column exists and returns column value at the given index 
+
+    This function is used mostly to check for whether a column for excel
+    worksheet data exists in a particular data location list data frame.
+
+    Args:
+        df (pandas.dataframe): The data frame to check
+        col (str): the column name to check for
+        index (str or int): the row index to get column value for, if column
+            exists.
+
+    Returns:
+        Value at index, column in the dataframe, if column exists, otherwise
+        returns None
+
+    """
+
+    if col in df:
+        if type(df.loc[index, col]) is str:
+            return df.loc[index, col]
+
+    return None
+
+
+def is_energy(string, energy_strings=dat.energy_flows):
+    """is_energy(string, energy_strings=dat.energy_flows)
+    checks if a string refers to an energy flow
+
+    Args:
+        string(str): the string to check
+        energy_strings(list): list of strings that mark whether the substance
+            name that starts or ends with them is an energy flow.
+    Returns:
+        bool. True, if the string starts or ends with the energy marker. Otherwise, False.
+    
+    """
+
+    clean_string = clean_str(string)
+    is_it_energy = False
+    for string in energy_strings:
+        if clean_string.startswith(string) or clean_string.endswith(string):
+            is_it_energy = True
+        
+    return is_it_energy
+
+
+# DATA FRAME CONSTRUCTORS 
 
 def make_df(data, sheet=None, sep='\t', index=0, metaprefix = "meta", 
             col_order = False, T = False, drop_zero=False, sort=False):
@@ -63,6 +183,7 @@ def make_df(data, sheet=None, sep='\t', index=0, metaprefix = "meta",
         The generated dataframe.
 
     """
+
     if isinstance(data, pan.DataFrame):
         df = data
     elif not isinstance(data, str): 
@@ -99,83 +220,7 @@ def make_df(data, sheet=None, sep='\t', index=0, metaprefix = "meta",
     return df
 
 
-def check_for_col(df, col, index):
-    """ Checks if a column exists and returns column value at the given index 
-
-    This function is used mostly to check for whether a column for excel
-    worksheet data exists in a particular data location list data frame.
-
-    Args:
-        df (pandas.dataframe): The data frame to check
-        col (str): the column name to check for
-        index (str or int): the row index to get column value for, if column
-            exists.
-
-    Returns:
-        Value at index, column in the dataframe, if column exists, otherwise
-        returns None
-    """
-    if col in df:
-        if type(df.loc[index, col]) is str:
-            return df.loc[index, col]
-
-    return None
-
-
-def build_filedir(filedir, subfolder=None, file_id_list=[], time=True):
-    """ Builds complicated file directory names.
-    Used for allowing file to be output to unique directories.
-
-    Args:
-        filedir (str): the base file directory
-        subfolder (str): optional subfolder(s)
-            (Defaults to None.)
-        file_id_list (list of str): list of strings to append to the directory
-            name. 
-            (Defaults to an empty list.)
-        time (bool): Whether to include a date-time stamp in the file
-            directory  name. 
-            (Defaults to True.)
-
-    Returns:
-        The built file directory string.
-    """
-    if subfolder is not None:
-        filedir=f'{filedir}/{subfolder}'
-    
-    for file_id in file_id_list:
-        if file_id:
-            filedir = f'{filedir}_{file_id}'
-
-    if time is True:
-        filedir = f'{filedir}_{datetime.now().strftime("%Y-%m-%d_%H%M")}'
-
-    return filedir
-
-
-def is_energy(string, energy_strings=dat.energy_flows):
-    """is_energy(string, energy_strings=dat.energy_flows)
-    checks if a string refers to an energy flow
-
-    Args:
-        string(str): the string to check
-        energy_strings(list): list of strings that mark whether the substance
-            name that starts or ends with them is an energy flow.
-    Returns:
-        bool. True, if the string starts or ends with the energy marker. Otherwise, False.
-    
-    """
-
-    clean_string = clean_str(string)
-    is_it_energy = False
-    for string in energy_strings:
-        if clean_string.startswith(string) or clean_string.endswith(string):
-            is_it_energy = True
-        
-    return is_it_energy
-
-
-def mass_energy_df(df, energy_strings=dat.energy_flows, totals=True):
+def mass_energy_df(df, energy_strings=dat.energy_flows, totals=True, units=dat.default_units):
     """Reorders dataframe to seperate mass and energy flows
 
     Uses a list of prefix/suffixes to identify mass and energy flows
@@ -188,6 +233,8 @@ def mass_energy_df(df, energy_strings=dat.energy_flows, totals=True):
             (Defaults to dat.energy_flows)
         totals (bool): Appends summation rows for mass and energy seperately.
             (Defaults to True)
+        units (dict): dictionary with keys of "mass" and "energy", and
+            values for what the units used for each of those are
     """
     logger.debug(f"seperating mass and energy flows using {energy_strings} as energy flow markers")
 
@@ -219,50 +266,49 @@ def mass_energy_df(df, energy_strings=dat.energy_flows, totals=True):
 
     if totals is True:
         if not mass_df.empty:
-            mass_df = mass_df.append(mass_df.sum().rename('TOTAL - mass'))
+            mass_df = mass_df.append(mass_df.sum().rename(f'TOTAL MASS, in {units["mass"]}'))
         if not energy_df.empty:
-            energy_df = energy_df.append(energy_df.sum().rename('TOTAL - energy'))
+            energy_df = energy_df.append(energy_df.sum().rename(f'TOTAL ENERGY, in {units["energy"]}'))
 
     combined_df = pan.concat([mass_df, energy_df], keys=['Mass', 'Energy'])
 
     return combined_df
     
-def metadata_df(user=dat.user_data, name="unknown", level="unknown", 
+
+def metadata_df(user=dat.user_data, about=about.about_blackblox, name="unknown", level="unknown", 
                 product="unknown", product_qty="unknown", scenario="unknown",
-                energy_flows=dat.energy_flows):
+                energy_flows=dat.energy_flows, units = dat.default_units):
+    """Generates a metadata dataframe for use in excel file output
+    """
     
-    about = {"name": "BlackBlox.py",
-          "version": "0.1",
-          "URL": "OFFLINE",
-          "documentation": "OFFLINE",
-          "github": "OFFLINE",
-          "creator": "S.E. Tanzer",
-          "affiliation": "Energy & Industry Group, Department of Engineering Systems and Services, Facult of Technology Policy, and Management, Delft University of Technology, The Netherlands",
-          "license": "GPL v3",
-         }
+    if is_energy(product):
+        product_type = 'energy'
+    else:
+        product_type = 'mass'
               
     creation_date = datetime.now().strftime("%A, %d %B %Y at %H:%M")
     energy_flows = ', '.join(energy_flows)
 
     meta = {"00": f"This data was calculated using {about['name']} v{about['version']}",
             "01": f"{about['name']} was created by {about['creator']} of {about['affiliation']}",
-            "02": f"More information on {about['name']} can be found at {about['URL']}",
+            "02": f"More information on {about['name']} can be found at {about['url']}",
             "03": " ",
             "04": " ",
             "05": f"This file was generated on {creation_date}",
             "06": f"by {user['name']} of {user['affiliation']}",
             "07": f"for use in {user['project']}",
             "08": f"and contains {level}-level results data for {name}",
-            "09": f"balanced on {product_qty} of {product} using the variable values from the {scenario} scenario(s)",
-            "10": " ",
-            "11": f"Note: Substances beginning or ending with any of the following strings were assumed by {about['name']} to be energy flows:",
-            "12": f"{energy_flows}",
-            "13": " ",
+            "09": f"balanced on {product_qty} {units[product_type]} of {product} using the variable values from the {scenario} scenario(s).",
+            "10": f"Mass quantites are given in {units['mass']} and energy quantities in {units['energy']}",
+            "11": " ",
+            "12": f"Note: Substances beginning or ending with any of the following strings were assumed by {about['name']} to be energy flows:",
+            "13": f"{energy_flows}",
             "14": " ",
-            "15": f"{about['name']} is a python package that faciliates the calculation of mass and energy balances for black block models at an arbitrary level of detail.",
-            "16": f"For full documentation on how to use {about['name']}, visit {about['documentation']}",
-            "17": f"{about['name']} is currently under active development. Head over to {about['github']} to download, fork, or contribute.",
-            "18": f"{about['name']} is avaiable for use free of charge under the terms and conditions of the {about['license']} license.",
+            "15": " ",
+            "16": f"{about['name']} is a python package that faciliates the calculation of mass and energy balances for black block models at an arbitrary level of detail.",
+            "17": f"For full documentation on how to use {about['name']}, visit {about['documentation_url']}",
+            "18": f"{about['name']} is currently under active development. Head over to {about['github_url']} to download, fork, or contribute.",
+            "19": f"{about['name']} is avaiable for use free of charge under the terms and conditions of the {about['license']} license.",
     }
 
     meta_df = pan.DataFrame.from_dict(meta, orient='index', columns=['Workbook Information'])
@@ -272,6 +318,39 @@ def metadata_df(user=dat.user_data, name="unknown", level="unknown",
     logger.debug(f"metadata dataframe created for {level}-level {name}")
 
     return meta_df
+
+
+# WRITERS TO FILES
+
+def build_filedir(filedir, subfolder=None, file_id_list=[], time=True):
+    """ Builds complicated file directory names.
+    Used for allowing file to be output to unique directories.
+
+    Args:
+        filedir (str): the base file directory
+        subfolder (str): optional subfolder(s)
+            (Defaults to None.)
+        file_id_list (list of str): list of strings to append to the directory
+            name. 
+            (Defaults to an empty list.)
+        time (bool): Whether to include a date-time stamp in the file
+            directory  name. 
+            (Defaults to True.)
+
+    Returns:
+        The built file directory string.
+    """
+    if subfolder is not None:
+        filedir=f'{filedir}/{subfolder}'
+    
+    for file_id in file_id_list:
+        if file_id:
+            filedir = f'{filedir}_{file_id}'
+
+    if time is True:
+        filedir = f'{filedir}_{datetime.now().strftime("%Y-%m-%d_%H%M")}'
+
+    return filedir
 
 
 def write_to_excel(df_or_df_list, sheet_list=None, filedir=dat.outdir, 
@@ -320,60 +399,43 @@ def write_to_excel(df_or_df_list, sheet_list=None, filedir=dat.outdir,
     logger.debug(f"{filename} successfully created in {filedir}")
 
 
-def clean_str(string_to_check, str_to_cut=False, lower=True, remove_dblnewline=True):
-    """Multipurpose function to clean user input strings
+def format_and_save_plot(filepath):
+   plt.style.use('tableau-colorblind10')
+   plt.save(f"{filepath}.png", format='png', dpi=300)
+   plt.save(f"{filepath}.svg", format='svg')
+
+
+def plot_annual_flows(df_dict, flow, outdir, unit_dict=dat.default_units):
+    flow_series = []
+    for df_name in df_dict:
+        if 'cumulative' in df_name:
+            pass
+        else:
+            df = df_dict[df_name].fillna(0)
+            if flow in df:
+                s = df.loc[:,flow]
+                s = s.rename(df_name)
+                flow_series.append(s)
     
-    Used to clean user input. First creates a copy of the string to prevent 
-    the original string from being modified unintentionally.
+    flow_df = pan.concat(flow_series, axis=1, sort=True)
+    df_index = flow_df.index.tolist()
 
-    Args:
-        string_to_check (str): The string to strip and lower
-        str_to_cut (str/list/bool): If passed a string, will check for and 
-            remove the cut_str string from the string_to_check. If passed a list 
-            of strings, will check for and remove each from the string_to_check.
-            (Defaults to False.)
-        remove_dblnewlines (bool): If True, looks for and removes instances
-            of double new lines.
-            (Defaults to True.)
-
-    Returns
-        The input string, stripped of leading and trailing white space 
-        and in all lowercase.
-    """
-    string = ''.join(string_to_check)
-
-    string = string.strip()
-    
-    if lower is True:
-        string = str.lower(string)
-
-    if type(str_to_cut) is str:
-        string = string.replace(str_to_cut, '')
-    if type(str_to_cut) is list:
-        for snip in str_to_cut:
-            string = string.replace(snip, '')
-
-    if remove_dblnewline is True:
-        if '\n\n' in string:
-            string = string.replace('\n\n', '\n')
-
-    return string
-
-
-def if_str(maybe_a_string):
-    """checks if an input is a string, returns string if so, None if not.
-
-    Args:
-        string: a thing to check if it's a string.
-    
-    Returns:
-        The string, if it was a string, otherwise returns None.
-    """
-    if type(maybe_a_string) is str:
-        return maybe_a_string
+    energy_flow = is_energy(flow)
+    if energy_flow is True:
+        flow_unit = unit_dict['energy']
     else:
-        return None
+        flow_unit = unit_dict['mass']
 
+    #plt.style.use('tableau-colorblind10')
+    flow_df.plot(title=f"annual outflows of {flow}")
+    plt.xticks(list(range(len(df_index))), df_index, rotation=90)
+    plt.ylabel(f"{flow_unit} {flow}")
+
+    format_and_save_plot(f'{outdir}/{flow}')
+   # plt.savefig(f'{outdir}/{flow}')
+
+
+# MISCELLANEOUS FUNCTIONS
 
 def nested_dicts(levels=2, final=float):
     """Created a nested defaultdict with an arbitrary level depth
@@ -398,39 +460,4 @@ def nested_dicts(levels=2, final=float):
     """
     return (defaultdict(final) if levels <2 else
             defaultdict(lambda: nested_dicts(levels - 1, final)))
-
-
-def plot_annual_flows(df_dict, flow, outdir, unit_dict=dat.default_units):
-    flow_series = []
-    for df_name in df_dict:
-        if 'cumulative' in df_name:
-            pass
-        else:
-            df = df_dict[df_name].fillna(0)
-            if flow in df:
-                s = df.loc[:,flow]
-                s = s.rename(df_name)
-                flow_series.append(s)
-    
-    flow_df = pan.concat(flow_series, axis=1, sort=True)
-    df_index = flow_df.index.tolist()
-
-    energy_flow = is_energy(flow)
-    if energy_flow is True:
-        flow_unit = unit_dict['energy']
-    else:
-        flow_unit = unit_dict['mass']
-
-    plt.style.use('tableau-colorblind10')
-    flow_df.plot(title=f"annual outflows of {flow}")
-    plt.xticks(list(range(len(df_index))), df_index, rotation=90)
-    plt.ylabel(f"{flow_unit} {flow}")
-    plt.savefig(f'{outdir}/{flow}')
-
-# def plot_df(df, columns, kind='line', outdir)
-#     column_series = []
-#     df = df.fillna(0)
-#     for col in columns:
-#         if col in df:
-#             s = df.loc[:,col]
 
