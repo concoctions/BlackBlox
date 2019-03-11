49776397d7b72aef5b775c3390ac72b41a1248a4
Wajju <setanzer@gmail.com>
2019-03-11 23:38:36 +0100

    minor fixes to demo

--- demo.py
+++ demo.py
@@ -24,6 +24,9 @@ dat.default_units = {'mass': 'tonnes',
 }
 
 print("\n\nWELCOME\n\n")
+
+input("\n\n\nPress enter to start demo. \n")
+
 print(f"\nblackblox.py v0.0.2 Demonstration")
 print(f"{datetime.now().strftime('%A, %d %B %Y at %H:%M')}")
 
@@ -32,34 +35,31 @@ print(f"and outputting any files to {dat.outdir}")
 
 print('\nNOTE: Throughout this demo, directly pressing ENTER moves you forward one step,\n' 
         'while entering a character skips to the next part of the demonstration')
-
-input("\n\n\nPress enter to start demo. \n")
     
 ##############################################################################
 ## UNIT PROCESS TEST
 ##############################################################################
 print('\n\n\nUNIT PROCESS TEST - outputs to console.')
 
-stop = input("\nPress enter to balance a random unit process: ")
+stop = input("\nPress enter to balance the KILN unit process: ")
 
 while stop == '':
     if stop != '':
         break
     unit_list =uni.df_unit_library.index.tolist()
-    random_unit = random.choice(unit_list)
-    random_unit = uni.UnitProcess(random_unit)
+    kiln = uni.UnitProcess('kiln')
     qty = round(random.uniform(10, 1000),2)
-    product = random_unit.default_product
+    product = kiln.default_product
 
-    scenarios = random_unit.var_df.index.tolist()
+    scenarios = kiln.var_df.index.tolist()
     s = random.choice(scenarios)
 
-    print("\n",str.upper(random_unit.name))
-    print("\ninflows:", ', '.join(random_unit.inflows))
-    print("outflows:", ', '.join(random_unit.outflows))
-    print(f"\nbalacing {random_unit.name} on {qty} of {product} ({random_unit.default_io}) using {s} values")
+    print("\n",str.upper(kiln.name))
+    print("\ninflows:", ', '.join(kiln.inflows))
+    print("outflows:", ', '.join(kiln.outflows))
+    print(f"\nbalacing {kiln.name} on {qty}  of {product} ({kiln.default_io}) using {s} values")
 
-    u_in, u_out = random_unit.balance(qty, scenario=s)
+    u_in, u_out = kiln.balance(qty, scenario=s)
     print("\ncalculated inflow quantities:")
     for k, v in u_in.items():
         print(f'  {k}: {v}')
@@ -67,19 +67,19 @@ while stop == '':
     for k, v in u_out.items():
         print(f'  {k}: {v}')
 
-    stop = input(f"\n\n\nPress enter to balance {random_unit.name} on a random inflow: ")
+    stop = input(f"\n\n\nPress enter to balance {kiln.name} on a random inflow: ")
     if stop != '':
         break
 
     print(f"\nselecting random inflow...")
-    product = random.choice(tuple(random_unit.inflows))
+    product = random.choice(tuple(kiln.inflows))
     if product == 'fuel':
-            qty = u_in[random_unit.var_df.at[s, 'fuelType']]
+            qty = u_in[kiln.var_df.at[s, 'fuelType']]
     else:
         qty = u_in[product]
-    print(f"\nnow balacing {random_unit.name} on {qty} of {product} ({'inflow'}) using {s} values")
+    print(f"\nnow balacing {kiln.name} on {qty} {dat.default_units['mass']} of {product} ({'inflow'}) using {s} values")
 
-    u1_in, u1_out = random_unit.balance(qty, product, 'i', s) 
+    u1_in, u1_out = kiln.balance(qty, product, 'i', s) 
         
     rounded_u_in = {k:round(v, 6) for k, v in u_in.items()}
     rounded_u_out = {k:round(v, 6) for k, v in u_out.items()}
@@ -104,7 +104,7 @@ while stop == '':
     max_replace_fraction = round(random.uniform(0.01, 1.0),2)
     print(f"\nRecycling {qty_recycled} of RECYCLED FLOW 1-to1 for not more than {max_replace_fraction*100}% of {product}") 
 
-    r1_in, r1_out, leftover = random_unit.recycle_1to1(original_inflows_dict=u_in,
+    r1_in, r1_out, leftover = kiln.recycle_1to1(original_inflows_dict=u_in,
                                                        original_outflows_dict=u_out,
                                                        recycled_qty=qty_recycled,
                                                        recycle_io='i',
@@ -125,69 +125,39 @@ while stop == '':
         for k, v in r1_out.items():
             print(f'  {k}: {v}')
     print(f"\nRECYCLE FLOW leftover: {leftover}")
-
-    if 'fuel' in random_unit.inflows:
-            fuel_type = random_unit.var_df.at[s, 'fuelType']
-            fuel_qty =  u_in[random_unit.var_df.at[s, 'fuelType']]
-            recycled_energy_qty = round(fuel_qty * random.uniform(30,100),2)
-            max_replace_fraction = round(random.uniform(0.01, 1.0),2)
-
-            print(f"\nalso testing recycle_energy_replacing_fuel on {fuel_type} (inflow)...")
-
-            print(f"\nRecycling {recycled_energy_qty} of RECYCLED ENERGY into {random_unit.name} replacing no more than {max_replace_fraction*100}% of {fuel_type})") 
-
-            r1_in, r1_out, leftover = random_unit.recycle_energy_replacing_fuel(original_inflows_dict=u_in,
-                                                                                original_outflows_dict=u_out,
-                                                                                recycled_qty=recycled_energy_qty,
-                                                                                recycle_io="inflow",
-                                                                                recycled_flow="RECYCLED ENERGY",
-                                                                                replaced_flow=fuel_type,
-                                                                                max_replace_fraction=max_replace_fraction,
-                                                                                combustion_eff = dat.combustion_efficiency_var,
-                                                                                scenario=s,
-                                                                                emissions_list = dat.default_emissions)
-
-            print("\nrecalculated inflow quantities:")
-            for k, v in r1_in.items():
-                print(f'  {k}: {v}')
-            print("\nrecalculated outflow quantities:")
-            for k, v in r1_in.items():
-                print(f'  {k}: {v}')
-            print(f"\nRECYCLE FLOW leftover: {leftover}")
-            print(f"\nNote: both 1-to-1 recyle and energy-replacing-fuel recycle shown above.")
             
-
-    stop = input(f"\n\n\nPress enter to balance {random_unit.name} on a random outflow"
+    stop = input(f"\n\n\nPress enter to balance {kiln.name} on a random outflow"
         "\nusing its quantity from the initial balance: ")
     if stop != '':
         break
 
     print(f"\nselecting random outflow...")
-    product = random.choice(tuple(random_unit.outflows))
+    product = random.choice(tuple(kiln.outflows))
+    while product == 'slag':
+        print('cannot balance on slag, because slag is calculated as a remainder of an inflow and outflow. Selecting alternate outflow.')
+        product = random.choice(tuple(kiln.outflows))
     if product == 'fuel':
-            qty = u_out[random_unit.var_df.at[s, 'fuelType']]
-    if product == 'slag':
-        print('cannot balance on slag, because slag is calculated as a remainder of an inflow and outflow. Skipping for now.')
-    else:
-        qty = u_out[product]
-        print(f"\nnow balacing {random_unit.name} on {qty} of {product} ({'outflow'}) using {s} values")
+            qty = u_out[kiln.var_df.at[s, 'fuelType']]
 
-        u1_in, u1_out = random_unit.balance(qty, product, 'o', s) 
-            
-        rounded_u1_in = {k:round(v, 6) for k, v in u1_in.items()}
-        rounded_u1_out = {k:round(v, 6) for k, v in u1_out.items()}
-        if rounded_u1_in == rounded_u_in and rounded_u1_out == rounded_u_out:
-            print("\nSame input/output as above, rounded to 6 decimal places")
-        else:
-            print("\n [!] not equivelent to above [!]\n")
-            print("\ncalculated inflow quantities:")
-            for k, v in r1_in.items():
-                print(f'  {k}: {v}')
-            print("\ncalculated outflow quantities:")
-            for k, v in r1_out.items():
-                print(f'  {k}: {v}')
-            if product in dat.default_emissions or product == 'waste heat':
-                print(f"\n(Since this was {product}, which has multiple sources, this was expected.)")
+    qty = u_out[product]
+    print(f"\nnow balacing {kiln.name} on {qty} of {product} ({'outflow'}) using {s} values")
+
+    u1_in, u1_out = kiln.balance(qty, product, 'o', s) 
+        
+    rounded_u1_in = {k:round(v, 6) for k, v in u1_in.items()}
+    rounded_u1_out = {k:round(v, 6) for k, v in u1_out.items()}
+    if rounded_u1_in == rounded_u_in and rounded_u1_out == rounded_u_out:
+        print("\nSame input/output as above, rounded to 6 decimal places")
+    else:
+        print("\n [!] not equivelent to above [!]\n")
+        print("\ncalculated inflow quantities:")
+        for k, v in r1_in.items():
+            print(f'  {k}: {v}')
+        print("\ncalculated outflow quantities:")
+        for k, v in r1_out.items():
+            print(f'  {k}: {v}')
+        if product in dat.default_emissions or product == 'waste heat':
+            print(f"\n(Since this was {product}, which has multiple sources, this was expected.)")
 
         stop = input(f"\n\n\nPress enter to test 1-to-1 recycle on {product} (outflow): ")
         if stop != '':
@@ -197,7 +167,7 @@ while stop == '':
         max_replace_fraction = round(random.uniform(0.01, 1),2)
         print(f"\nRecycling {qty_recycled} of RECYCLED FLOW 1-to1 for not more than {max_replace_fraction}% of {product}") 
 
-        r1_in, r1_out, leftover = random_unit.recycle_1to1(original_inflows_dict=u_in,
+        r1_in, r1_out, leftover = kiln.recycle_1to1(original_inflows_dict=u_in,
                                                            original_outflows_dict=u_out,
                                                            recycled_qty=qty_recycled,
                                                            recycle_io='o',
@@ -219,9 +189,36 @@ while stop == '':
             print(f'  {k}: {v}')
         print(f"\nRECYCLE FLOW leftover: {leftover}")
 
-    stop = input("\n\n\nPress enter to balance another random unit process or enter a character to move on: ")
-    if stop != '':
-        break
+    fuel_type = kiln.var_df.at[s, 'fuelType']
+    fuel_qty =  u_in[kiln.var_df.at[s, 'fuelType']]
+    recycled_energy_qty = round(fuel_qty * random.uniform(30,100),2)
+    max_replace_fraction = round(random.uniform(0.01, 1.0),2)
+
+    stop = input(f"\n\n\nPress enter to recycle energy to replace {fuel_type} (inflow) in  {kiln.name}")
+
+
+    print(f"\nRecycling {recycled_energy_qty} {dat.default_units['energy']} of RECYCLED ENERGY into {kiln.name} replacing no more than {max_replace_fraction*100}% of {fuel_type})") 
+
+    r1_in, r1_out, leftover = kiln.recycle_energy_replacing_fuel(original_inflows_dict=u_in,
+                                                                        original_outflows_dict=u_out,
+                                                                        recycled_qty=recycled_energy_qty,
+                                                                        recycle_io="inflow",
+                                                                        recycled_flow="RECYCLED ENERGY",
+                                                                        replaced_flow=fuel_type,
+                                                                        max_replace_fraction=max_replace_fraction,
+                                                                        combustion_eff = dat.combustion_efficiency_var,
+                                                                        scenario=s,
+                                                                        emissions_list = dat.default_emissions)
+
+    print("\nrecalculated inflow quantities:")
+    for k, v in r1_in.items():
+        print(f'  {k}: {v}')
+    print("\nrecalculated outflow quantities:")
+    for k, v in r1_in.items():
+        print(f'  {k}: {v}')
+    print(f"\nRECYCLE FLOW leftover: {leftover}")
+
+    stop = 'stop'
     
 
 ##############################################################################
@@ -459,4 +456,6 @@ while stop == '':
     print("\nDone. Files available in demo_output directory.")
     stop = 'stop'
 
-print('\n\n\nEND OF DEMO.\n\n')
+input("\n\n\nPress enter to end demo. \n")
+
+print('\n\n\nGOOD BYE.\n\n')
