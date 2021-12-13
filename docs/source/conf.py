a604685b69d48e63154f9e0b9aa514179851023f
Wajju <47030036+concoctions@users.noreply.github.com>
2021-12-13 01:11:28 +0100

    sphinx rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,7 @@ extensions = [
     'myst_parser',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
 ]
 
 source_suffix = ['.rst', '.md']
@@ -53,7 +54,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
