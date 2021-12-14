9b4afff4986e5a566e8bbd3fd52db058f665bda6
João Paulo Pizani Flor <paulopizani@posteo.net>
2021-12-14 12:27:23 +0100

    Adaptations to pyproject.toml and docs/source/conf.py to make docs build. Still issues

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,16 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 import mock
 
-MOCK_MODULES = ['numpy', 'graphviz', 'molmass', 'matplotlib', 'pandas', 'xlrd', 'openpyxl', 'seaborn']
+MOCK_MODULES = [
+    'numpy.linalg',
+    'graphviz',
+    'molmass',
+    'matplotlib.pyplot',
+    'pandas',
+    'xlrd',
+    'openpyxl',
+    'seaborn',
+]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
