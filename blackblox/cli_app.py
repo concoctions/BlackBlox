9815f36f6cf80ef33aeae5ef52d870df429ab03a
Wajju <47030036+concoctions@users.noreply.github.com>
2022-04-10 01:53:21 +0200

    added --get_defaults
    added command to list (most of) the available bbcfg config arguments and their default values in the terminal window
    
    also: now each command has a 1-letter argument and  a "full name" argument

--- blackblox/cli_app.py
+++ blackblox/cli_app.py
@@ -1,21 +1,51 @@
 import argparse
-from pathlib import Path
+from pathlib import Path, PosixPath
+import pprint
+from types import NoneType
 
 from blackblox.parse_scenario import run_scenario_file
 import blackblox.unitprocess as uni
 import blackblox.factory as fac
+from blackblox.dataconfig import bbcfg
 
 
 def main():
     parser = argparse.ArgumentParser(description='Use the BlackBlox library to run a scenario described in a config file (YAML).')
-    parser.add_argument('--config', help='Path to scenario config file (default: \'./config.yaml\')')
-    parser.add_argument('--u', help='Specify unit by unit_id from unit library. Runs unit specified on default parameters.')
-    parser.add_argument('--f', help='Specify filename of a factory Excel file with sheets "chains" and "connections". Runs unit specified on default parameters.')
-    parser.add_argument('--s', help='Specify scenario for --u or --f command.')
-    parser.add_argument('--q', help='Specify product qty for --u or --f command.')
+    parser.add_argument('--config', '--c', help='Path to scenario config file (default: \'./config.yaml\')')
+    parser.add_argument('--u', '--unit', help='Specify unit by unit_id from unit library. Runs unit specified on default parameters.')
+    parser.add_argument('--f', '--factory', help='Specify filename of a factory Excel file with sheets "chains" and "connections". Runs unit specified on default parameters.')
+    parser.add_argument('--s', '--scenario', help='Specify scenario for --u or --f command.')
+    parser.add_argument('--q', '--qty', help='Specify product qty for --u or --f command.')
+    parser.add_argument('--d', '--get_defaults', action='store_true', help='displays YAML-configurble attributes and their defaults')
 
     args = parser.parse_args()
 
+
+    if args.d:
+        for i, j in [('user', bbcfg.user), 
+                     ('scenario_default', bbcfg.scenario_default),
+                     ('paths', bbcfg.paths),
+                     ('columns', bbcfg.columns),
+                     ('emissions', bbcfg.emissions),
+                     ('fuel_flows', bbcfg.fuel_flows),
+                     ('shared_var.path_shared_fuels', bbcfg.shared_var.path_shared_fuels),
+                     ('shared_var.path_shared_upstream', bbcfg.shared_var.path_shared_upstream),                     
+                     ]:
+            if isinstance(j, (float, str, int, list, Path, PosixPath, NoneType)):
+                if isinstance(j, str):
+                    print_v = f'\'{j}\''
+                else:
+                    print_v = j
+                print(f'\n{i}: {print_v}')
+            else:
+                print('\n', i)
+                for k, v, in vars(j).items():
+                    if isinstance(v, str):
+                        print_v = f'\'{v}\''
+                    else:
+                        print_v = v
+                    print(f'{k}: {print_v}')
+        print('\n')
 
 
 
